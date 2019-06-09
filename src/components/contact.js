1264dc33aa66ec08cac7b16462ab70439ebc5dcd
GBJersey <GBJersey@github.com>
2019-06-09 11:52:58 +0200

    fix display issue

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,7 +12,8 @@ class Contact extends Component {
                     <h2>Aleksander Gis</h2>
                     <img 
                             src={avatar}
-                            alt='avatar'
+                            alt="avatar"
+                            className="contact-avatar"
                        />
                     <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Do czego tego użyć?
 Ogólnie znana teza głosi, iż użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, sprawiającego, że wygląda to „zbyt czytelnie” po polsku. Wielu webmasterów i designerów używa Lorem Ipsum jako domyślnego modelu tekstu i wpisanie w internetowej wyszukiwarce ‘lorem ipsum’ spowoduje znalezienie bardzo wielu stron, które wciąż są w budowie. Wiele wersji tekstu ewoluowało i zmieniało się przez lata, czasem przez przypadek, czasem specjalnie (humorystyczne wstawki itd).</p>
